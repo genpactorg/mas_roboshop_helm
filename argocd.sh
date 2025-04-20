e1ba82837bb0dc448605540dc4e67a6918ca180f
mas123shaik <masthan.shaik@gmail.com>
2025-04-20 10:19:14 +0530

    github action changes

--- argocd.sh
+++ argocd.sh
@@ -5,7 +5,8 @@ imageTag=$3
 
 
 PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
-argocd login argocd-${env}.masdevops.online --grpc-web --insecure --username admin --password $PASSWORD
+#argocd login argocd-${env}.masdevops.online --grpc-web --insecure --username admin --password $PASSWORD
+argocd login a4b2d301859ad40e2afa74b14ff65cee-1992582938.us-east-1.elb.amazonaws.com --grpc-web --insecure --username admin --password $PASSWORD
 
 
 argocd app create ${component_name} --upsert --repo https://github.com/raghudevopsb83/roboshop-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
